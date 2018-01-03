8df86e6ba8c2cb0d42038fcff1cd8c53fda303bd
Ken Huynh <kienhht@ilandapp.com>
2018-01-03 20:53:41 +0700

    Add icon launcher

--- XFFinanceManager/XFFinanceManager.Android/MainActivity.cs
+++ XFFinanceManager/XFFinanceManager.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using SegmentedControl.FormsPlugin.Android;
 
 namespace XFFinanceManager.Droid
 {
-    [Activity(Label = "iMoney", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "iMoney", Icon = "@drawable/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
